086bf91d4d968005024fc6198e889df3327dd539
Francois Caicedo <FrancisToutSeul@gmail.com>
2018-10-25 17:32:37 +0200

    random algo OK

--- compile.py
+++ compile.py
@@ -1,8 +1,10 @@
 import os
 
 PATH = "C:\\Users\\Francis\\AppData\\Local\\Programs\\Python\\Python36-32\\Scripts"
-SRCS = "srcs/"
-PY_FILE = SRCS + "bot.py " + SRCS + "pisqpipe.py --name pbrain-bordeaux-francois.caicedo.exe --onefile"
+SRCS = "srcs\\"
+NAME = "pbrain-bordeaux-francois.caicedo.exe"
+PY_FILE = SRCS + "bot.py " + SRCS + "pisqpipe.py --name " + NAME + " --onefile"
 
 print(PY_FILE)
 os.system("pyinstaller -p " + PATH + " " + PY_FILE)
+os.system("copy dist\\" + NAME + " .")
