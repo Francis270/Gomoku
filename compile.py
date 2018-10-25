dce7d7f5afe372cdb9b9d445b12a69fcec046982
Francois Caicedo <FrancisToutSeul@gmail.com>
2018-10-25 16:47:05 +0200

    initial commit

--- compile.py
+++ compile.py
@@ -1,5 +1,7 @@
 import os
 
-PY_FILE = "\"pbrain-Gomoku francois.caicedo@epitech.eu.py\""
+SRCS = "srcs/"
+PY_FILE = SRCS + "bot.py " + SRCS + "pisqpipe.py --name pbrain-bordeaux-francois.caicedo.exe --onefile"
 
+print(PY_FILE)
 os.system("pyinstaller " + PY_FILE)
--- pbrain-Gomoku francois.caicedo@epitech.eu.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def main():
-	print("GOMOKU BOT")
-	input()
-
-if __name__ == "__main__":
-    main()
